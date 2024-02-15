5435666bec4285150cd338eb5ab8859aafae0729
Logan <lrfoster03@gmail.com>
2024-02-14 18:18:45 -0800

    optimized

--- calculator.py
+++ calculator.py
@@ -1,14 +1,15 @@
-input = input("Enter a number: ")
 sequence = []
 
 def computeSequence(input):
     sequence.append(input.__int__())
     if input == 1:
-        return 1
+        return 
     elif input % 2 == 0:
         return computeSequence(input / 2)
     else:
         return computeSequence(3 * input + 1)
     
+input = input("Enter a number: ")
+
 computeSequence(int(input))
 print(sequence)
