2ef97a27471f4d0d62b76784d0dd7d1f5149e517
Logan <lrfoster03@gmail.com>
2024-02-18 01:42:26 -0800

    Can now represent multiple collatz sequences on the same graph

--- calculator.py
+++ calculator.py
@@ -5,6 +5,7 @@ sequence = []
 G = nx.DiGraph()
 
 def computeSequence(input):
+    # Clear the sequence before computing a new one
     sequence.append(input.__int__())
     if input == 1:
         return 
@@ -17,17 +18,14 @@ def plotSequence():
     for i in range(len(sequence) - 1):
         G.add_edge(str(sequence[i]), str(sequence[i+1]))
 
-    val_map = {"1": 1.0,
-           "2": 0.6,
-           "4": 0.8}
+    # Add error handling in the event that the user enters a number that is less than 4 (aka 2 or 1)
+
     values = [val_map.get(node, 0.75) for node in G.nodes()]
-    G.add_edge("1", "4")
     # Specify the edges you want here
     red_edges = [("4", "2"), ("2", "1"), ("1", "4")]
     edge_colours = ['black' if not edge in red_edges else 'red'
                     for edge in G.edges()]
     black_edges = [edge for edge in G.edges()]
-
     # Need to create a layout when doing
     # separate calls to draw nodes and edges
     pos = nx.spring_layout(G)
@@ -37,15 +35,29 @@ def plotSequence():
     nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
     nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=False)
     plt.show()
-    
-    
-input = input("Enter a number: ")
-input = int(input)
-if(input > 0):
-    computeSequence(input)
-    print("Completed in " + str(len(sequence)) + " iterations.")
-    print(sequence)
-else:
-    print("Invalid input")
 
-plotSequence()
+# These edges will always exist in a collatz sequence (As 1-> 4 -> 2 -> 1 is a cycle)
+G.add_edges_from([("1", "4"), ("4", "2"), ("2", "1")])
+val_map = {"1": 1.0,
+        "2": 0.6,
+        "4": 0.8}
+
+
+response = input("Enter a number, or type exit() to quit: ")
+while True:
+    if(response == "exit()"):
+        break
+    elif(type(response) is str and response.isdigit()):
+        response = int(response)
+        if(response > 0):
+            sequence = []
+            computeSequence(response)
+            print("Completed in " + str(len(sequence)) + " iterations.")
+            print(sequence)
+            plotSequence()
+        else:
+            print("Invalid input")
+    else:
+        # print("Invalid input")
+
+        response = input("Enter another number, or type exit() to quit: ")
